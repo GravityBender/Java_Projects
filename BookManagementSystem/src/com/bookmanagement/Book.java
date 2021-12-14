e8f5b2b57be6eb6c3c75d7359796a33019eafede
Garvit Verma <83156804+GravityBender@users.noreply.github.com>
2021-12-14 18:56:56 +0530

    Add files via upload

---
+++ BookManagementSystem/src/com/bookmanagement/Book.java
@@ -0,0 +1,36 @@
+package com.bookmanagement;
+
+public class Book {
+    private String authorName;
+    private String bookName;
+
+    public Book() {
+
+    }
+
+    public Book(String authorName, String bookName) {
+        this.authorName = authorName;
+        this.bookName = bookName;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    @Override
+    public String toString() {
+        return ("Book Name: " + this.bookName + "(" + this.authorName + ")");
+    }
+}
