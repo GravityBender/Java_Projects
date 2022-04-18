69c1560f809bf9f46d5886f2cfb8088e7e3ce57f
Garvit Verma <83156804+GravityBender@users.noreply.github.com>
2022-04-19 00:26:07 +0530

    Update App.java

--- N_Queen_GUI/src/App.java
+++ N_Queen_GUI/src/App.java
@@ -21,64 +21,59 @@ public class App extends Application {
 
     private static int ROWS = 8;
     private static int COLS = 8;
+    private static VBox vBox;
     private static GridPane chessGrid;
     private static Button startButton;
     private static Queens queen;
 
-    @Override
-    public void start(Stage window) {
+    private static Pane createSol() {
+        chessGrid = new GridPane();
+        chessGrid.getStylesheets().addAll("sol.css");
+        for (int i = 0; i < ROWS; i++) {
+            Node[] nodeList = new Node[COLS];
+            for (int j = 0; j < COLS; j++) {
+                Button btnNode = new Button();
+                btnNode.setPrefSize(90, 90);
+                nodeList[j] = btnNode;
 
-        HBox hBox = new HBox();
-        Text ques = new Text("Enter a number");
-        TextField textField = new TextField();
-        Button sbBtn = new Button("Enter");
-        hBox.getChildren().addAll(ques, textField, sbBtn);
-        hBox.setAlignment(Pos.CENTER);
-
-        sbBtn.setOnAction((e) -> {
-            String nString = textField.getText();
-            int n = Integer.parseInt(nString);
-
-            ROWS = n;
-            COLS = n;
-
-            VBox vBox = new VBox(8);
-            vBox.setPadding(new Insets(10));
-            vBox.setAlignment(Pos.CENTER);
-
-            startButton = new Button("Click to Start");
-            startButton.setPrefWidth(400);
-            startButton.setStyle("-fx-font-size: 2em");
-            vBox.getChildren().addAll(makeGrid(), startButton);
-
-            queen = new Queens(ROWS, COLS);
-            startButton.setOnAction((em) -> {
-                if (queen.solveQueen() == false) {
-                    Alert alertBox = new Alert(AlertType.INFORMATION);
-                    alertBox.setTitle("Alert!");
-                    alertBox.setHeaderText("ALERT!");
-                    alertBox.setContentText("No solution present!");
-                    alertBox.showAndWait();
-                } else {
-                    VBox nVBox = new VBox(8);
-                    nVBox.setPadding(new Insets(10));
-                    nVBox.setAlignment(Pos.CENTER);
-                    nVBox.getChildren().addAll(createSol());
-                    Scene answer = new Scene(nVBox);
-                    window.setScene(answer);
-                    window.setTitle("8-Queens Problem");
-                    window.show();
+                if (queen.getBoard()[i][j] == 1) {
+                    btnNode.setText("Q");
                 }
-            });
 
-            window.setScene(new Scene(vBox));
-            window.setTitle("8-Queens Problem");
-            window.show();
-        });
+                if (i % 2 == 0) {
+                    if (j % 2 == 0) {
+                        btnNode.getStyleClass().add("white-btn");
+                    } else {
+                        btnNode.getStyleClass().add("black-btn");
+                    }
+                } else {
+                    if (j % 2 == 0) {
+                        btnNode.getStyleClass().add("black-btn");
+                    } else {
+                        btnNode.getStyleClass().add("white-btn");
+                    }
+                }
+            }
+            chessGrid.addRow(i, nodeList);
+        }
+        return chessGrid;
+    }
 
-        window.setScene(new Scene(hBox));
-        window.setTitle("8-Queens Problem");
-        window.show();
+    private static VBox setNView() {
+        VBox nVBox = new VBox(8);
+        nVBox.setPadding(new Insets(10));
+        nVBox.setAlignment(Pos.CENTER);
+        nVBox.getChildren().addAll(createSol());
+
+        return nVBox;
+    }
+
+    private static void generateAlert() {
+        Alert alertBox = new Alert(AlertType.INFORMATION);
+        alertBox.setTitle("Alert!");
+        alertBox.setHeaderText("ALERT!");
+        alertBox.setContentText("No solution present!");
+        alertBox.showAndWait();
     }
 
     private static Pane makeGrid() {
@@ -111,37 +106,60 @@ public class App extends Application {
         return chessGrid;
     }
 
-    private Pane createSol() {
-        chessGrid = new GridPane();
-        chessGrid.getStylesheets().addAll("sol.css");
-        for (int i = 0; i < ROWS; i++) {
-            Node[] nodeList = new Node[COLS];
-            for (int j = 0; j < COLS; j++) {
-                Button btnNode = new Button();
-                btnNode.setPrefSize(90, 90);
-                nodeList[j] = btnNode;
+    private static VBox setVView() {
+        vBox = new VBox(8);
+        vBox.setPadding(new Insets(10));
+        vBox.setAlignment(Pos.CENTER);
 
-                if (queen.getBoard()[i][j] == 1) {
-                    btnNode.setText("Q");
-                }
+        startButton = new Button("Click to Start");
+        startButton.setPrefWidth(400);
+        startButton.setStyle("-fx-font-size: 2em");
+        vBox.getChildren().addAll(makeGrid(), startButton);
 
-                if (i % 2 == 0) {
-                    if (j % 2 == 0) {
-                        btnNode.getStyleClass().add("white-btn");
-                    } else {
-                        btnNode.getStyleClass().add("black-btn");
-                    }
+        queen = new Queens(ROWS, COLS);
+
+        return vBox;
+    }
+
+    @Override
+    public void start(Stage window) {
+
+        HBox hBox = new HBox();
+        Text ques = new Text("Enter a number");
+        TextField textField = new TextField();
+        Button sbBtn = new Button("Enter");
+        hBox.getChildren().addAll(ques, textField, sbBtn);
+        hBox.setAlignment(Pos.CENTER);
+
+        sbBtn.setOnAction((e) -> {
+            String nString = textField.getText();
+            int n = Integer.parseInt(nString);
+
+            ROWS = n;
+            COLS = n;
+
+            vBox = setVView();
+
+            startButton.setOnAction((em) -> {
+                if (queen.solveQueen() == false) {
+                    generateAlert();
                 } else {
-                    if (j % 2 == 0) {
-                        btnNode.getStyleClass().add("black-btn");
-                    } else {
-                        btnNode.getStyleClass().add("white-btn");
-                    }
+                    VBox nVBox = setNView();
+                    Scene answer = new Scene(nVBox);
+                    window.setScene(answer);
+                    window.setTitle("8-Queens Problem");
+                    window.show();
                 }
-            }
-            chessGrid.addRow(i, nodeList);
-        }
-        return chessGrid;
+            });
+
+            window.setScene(new Scene(vBox));
+            window.setTitle("8-Queens Problem");
+            window.show();
+        });
+
+        window.setScene(new Scene(hBox));
+        window.setTitle("8-Queens Problem");
+        window.show();
     }
 
     public static void main(String[] args) {
